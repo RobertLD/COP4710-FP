a36276380aa8ad898f6af59a0a6a8236209179c8
root <root@LAPTOP-ERVA00ON.localdomain>
2021-12-03 22:19:20 -0500

    Error handling for login and registration framework

--- frontend/html/index.php
+++ frontend/html/index.php
@@ -20,9 +20,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UCF Book Request - Login</title>
     <!-- JS imports-->
+    <script src="index.js" type="text/javascript"></script>
 </head>
 
-<body>
+<body onload="errorParse()" onhashchange="errorParse()">
+
+    <script type="text/javascript">
+        function errorParse(){
+            var url = window.location.href;
+
+            var errorVar = url.split("?");
+            console.log(errorVar);
+
+            if (errorVar.length == 1){
+                return;
+            }
+
+            var error = errorVar[1].split("=");
+            console.log(error);
+
+            var value = error[1];
+
+            var fields = document.getElementById("loginError");
+            var str = "";
+
+            // error=empty
+            if (value == "empty"){
+                str = "Please enter username and password.";
+            }
+            // error=uid
+            if (value == "uid"){
+                str = "Invalid Username.";
+            }
+            // error=uidstmt
+            if (value == "uidstmt"){
+                str = "Error preparing sql statement.";
+            }
+            // error=wronglogin
+            if (value == "wronglogin"){
+                str = "Invalid username or password.";
+            }
+            // error=none
+            if (value == "none"){
+                str = "";
+            }
+            // error=submit
+            if (value == "submit"){
+                str = "Error processing HTTP request.";
+            }
+
+            console.log(str);
+            fields.innerHTML = str;
+            fields.className = "row justify-content-center alert alert-danger";
+        }
+    </script>
+
     <div class="login">
         <div class="loginDiv">
             <form class="loginForm" action="../../backend/php/login.a.php" method="post">
@@ -35,7 +87,7 @@
             <a class="createAccountLink" href="register.html">Create New Account</a><br/>
             <a class="forgotPasswordLink" href="forgotPassword.html">Forgot Password?</a>
         </div>
-        <span class="loginError"></span>
+        <span id="loginError"></span>
     </div>
 </body>
 
